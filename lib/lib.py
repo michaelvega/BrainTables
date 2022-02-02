6cf85d73490b44bb3515cf7ea94ecde576267daa
Michael Vega <48607909+michaelvega@users.noreply.github.com>
2022-02-02 15:07:16 -0500

    Add files via upload

---
+++ lib/lib.py
@@ -0,0 +1,20 @@
+import random
+import math
+from kivymd.app import MDApp
+from kivymd.uix.screen import MDScreen
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivymd.uix.button import MDRaisedButton, MDRectangleFlatButton, MDFlatButton
+from kivy.uix.button import Button
+from kivymd.theming import ThemeManager
+from kivy.uix.textinput import TextInput
+from kivy.properties import NumericProperty
+from kivymd.uix.progressbar import MDProgressBar
+from kivy.clock import Clock
+from kivy.lang import Builder
+from kivymd.uix.behaviors.backgroundcolorbehavior import BackgroundColorBehavior
+from kivy.core.window import Window
+from kivy.animation import Animation
